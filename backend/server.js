dffa71388b6f12c6f2010b68928c1e16587c35b3
Nitesh-N-D <niteshdwaraka@gmail.com>
2026-03-11 00:12:24 +0530

    api domain changes

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,10 @@ connectDB();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://campus-air.vercel.app"
+    ],
     credentials: true
   })
 );
