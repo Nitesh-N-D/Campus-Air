ca8cd512f7802c5052c4f5130dc2c193b672af54
Nitesh-N-D <niteshdwaraka@gmail.com>
2026-03-11 00:57:09 +0530

    cookie

--- backend/server.js
+++ backend/server.js
@@ -39,9 +39,10 @@ app.use(
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
-    cookie: {
-      secure: false
-    }
+   cookie: {
+  secure: true,
+  sameSite: "none"
+}
   })
 );
 
