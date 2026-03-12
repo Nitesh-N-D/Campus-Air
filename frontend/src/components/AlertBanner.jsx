ad5c0da30db9ed19722e12d55ad672e570157131
Nitesh-N-D <niteshdwaraka@gmail.com>
2026-03-13 00:26:11 +0530

    alert banner

--- frontend/src/components/AlertBanner.jsx
+++ frontend/src/components/AlertBanner.jsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react";
 import API from "../services/api";
-import { X } from "lucide-react";
 import socket from "../services/socket";
+import { X } from "lucide-react";
+import { toast } from "react-toastify";
+
 function AlertBanner(){
 
   const [alert,setAlert] = useState(null);
@@ -27,17 +29,29 @@ function AlertBanner(){
 
       });
 
+    socket.on("newAlert", (data) => {
+
+      if(data.priority === "High" || data.priority === "Medium" || data.priority === "Low"){
+
+        setAlert(data);
+        setVisible(true);
+
+        toast.warning(
+          `🔔 New Alert: ${data.title}`,
+          {
+            position: "top-right",
+            autoClose: 5000
+          }
+        );
+
+      }
+
+    });
+
+    return () => socket.off("newAlert");
+
   },[]);
-  socket.on("newAlert", (data) => {
 
-  if(data.priority === "High"){
-    setAlert(data);
-    setVisible(true);
-  }
-
-});
-
-  // auto hide after 10 seconds
   useEffect(()=>{
 
     if(alert){
@@ -59,9 +73,27 @@ function AlertBanner(){
 
   if(!alert || !visible) return null;
 
+  /* priority styling */
+
+  let bg = "bg-blue-600";
+  let animate = "";
+
+  if(alert.priority === "High"){
+    bg = "bg-red-600";
+    animate = "animate-pulse";
+  }
+
+  if(alert.priority === "Medium"){
+    bg = "bg-yellow-500";
+  }
+
+  if(alert.priority === "Low"){
+    bg = "bg-blue-600";
+  }
+
   return(
 
-    <div className="bg-red-600 text-white rounded-md shadow mb-6">
+    <div className={`${bg} ${animate} text-white rounded-md shadow mb-6`}>
 
       <div className="flex justify-between items-center px-6 py-3">
 
@@ -73,6 +105,10 @@ function AlertBanner(){
 
           <span className="text-sm">
             {alert.message}
+          </span>
+
+          <span className="ml-3 text-xs opacity-80">
+            ({alert.priority})
           </span>
 
         </div>
